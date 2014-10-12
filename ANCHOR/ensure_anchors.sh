67fad2e02acb5cc2104032020a1b05788ae14580
brownman <brownman556@gmail.com>
2014-10-12 11:52:53 +0300

    [restore] docker-desktop

--- ANCHOR/ensure_anchors.sh
+++ ANCHOR/ensure_anchors.sh
@@ -9,7 +9,9 @@ test -f /tmp/library.cfg || { echo 1>&2 install the library first; exit 1; }
 source /tmp/library.cfg
 test -d "/tmp/dir_root" || exiting
 
+
 pushd $(dirname $0) >/dev/null
+#pushd $(where_am_i $0) >/dev/null
 while read line;do
   test -n "$line" || exit
   echo
--- ANCHOR/proxy.sh
+++ ANCHOR/proxy.sh
@@ -1,5 +1,32 @@
-clear
+#!/usr/bin/env bash
+#exec -c
+#set -x
+#set +e
+#trap_err(){
+#echo `caller`
+#}
+#trap trap_err ERR
+commander0(){
+local cmd="$@"
+echo "[cmd] $cmd" 
+eval "$cmd"
+}
+
+#clear
+reset_env(){
+#commander0 env - bash
+export SHELL=/bin/bash
+export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin:/usr/games
+export DISPLAY=:0 
+export LOGNAME=paretech
+export HOME=/home/paretech
+export TERM=/usr/sbin/xterm
+}
+
+
+
 ensure_install(){
+echo $FUNCNAME
   source /tmp/library.cfg
   use who_am_i
   use print
@@ -9,10 +36,22 @@ ensure_install(){
 }
 
 cmd="$@"
-    #notify-send -t 6000 -u critical "$str1" "$str2"
+
+echo $cmd > /tmp/cmd
+reset_env
+
+#set -x
+cmd=$(cat /tmp/cmd)
+echo [cmd] $cmd
+#sleep 3
+#gxmessage -file < env
 
 notify-send -t 1000 -u critical PROXY "$cmd" &
-
+#notify-send -t 6000 -u critical "$str1" "$str2"
 ensure_install
-( eval "$cmd" 2>/tmp/err ) || $gxmessage1
+
+#use dialog_confirm
+#dialog_confirm pilot? /tmp/pilot.sh
+
+( eval "$cmd" 2>/tmp/err ) && ( notify-send ok; ) || ( notify-send err; $gxmessage1 )
 #echo 2>&1  res: $res
